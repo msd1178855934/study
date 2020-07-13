81f93ef77372bb2fe54dad4c0b3e472526bbd0b1
msd1178855934 <1178855934@qq.com>
2020-07-13 10:26:20 +0000

    Synchronize

---
+++ leetcode/LRU_cache.js
@@ -0,0 +1,49 @@
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+    this.capacity = capacity;
+    this.map = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+    if (!this.map.has(key)) return -1;
+    const val = this.map.get(key);
+    this.map.delete(key);
+    this.map.set(key, val);
+    return val;
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+    this.map.delete(key);
+    this.map.set(key, value);;
+    if (this.map.size > this.capacity) this.map.delete(this.map.keys().next().value);
+};
+
+/** 
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
+
+const cache = new LRUCache( 2 /* capacity */ );
+
+cache.put(1, 1);
+cache.put(2, 2);
+console.log(cache.get(1));       // returns 1
+cache.put(3, 3);    // evicts key 2
+console.log(cache.get(2));       // returns -1 (not found)
+cache.put(4, 4);    // evicts key 1
+console.log(cache.get(1));       // returns -1 (not found)
+console.log(cache.get(3));       // returns 3
+console.log(cache.get(4));       // returns 4
